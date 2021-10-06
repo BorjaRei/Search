ccf397c2c46925eb9fe83619d3f89c8b343eaa44
JOrtuzar15 <jortuzar6@gmail.com>
2021-10-06 13:33:36 +0200

    fix: no pasa 2 veces por el mismo nodo

--- search.py
+++ search.py
@@ -132,16 +132,17 @@ def breadthFirstSearch(problem):
     while toEvaluate.isEmpty() == False and found == False:
         node = toEvaluate.pop()
         explored.add(node)
-        sucessors = problem.getSuccessors(node)
-        for i in sucessors:
-            n = i[0]
-
-            if problem.isGoalState(n):
-                found = True
-                goal = n
-            if n not in explored:
-                movements[n] = [i[1], node]
-                toEvaluate.push(n)
+        if problem.isGoalState(node):
+            found = True
+            goal = node
+        if not found:
+            sucessors = problem.getSuccessors(node)
+            for i in sucessors:
+                n = i[0]
+                if n not in explored:
+                    movements[n] = [i[1], node]
+                    if n not in toEvaluate.list:
+                        toEvaluate.push(n)
 
     sol = []
     next = goal
